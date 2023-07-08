7bc82f7e1bfe95e91dbd1b987921a5f5b370be8d
thomasAmara <thomastarawali@gmail.com>
2023-07-08 21:59:58 +0100

    Modal size

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  components: {
+    Modal: {
+      baseStyle: (props) => ({
+        dialog: {
+          marginTop: ['20px', '30px', '90px'],
+          maxWidth: ['280px', '300px', '400px', '850px'],
+          minWidth: ['200px', '200px', '350px', '780px '],
+          // bg: '#00ff00',
+        },
+      }),
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
