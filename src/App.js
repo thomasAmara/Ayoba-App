0bd44dc4b4db6e424003d3817575d887079a397d
thomasAmara <thomastarawali@gmail.com>
2023-07-10 00:10:52 +0100

    route path backend

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import User from './Users';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/backend' element={<User />} />
     </Routes>
   );
 }
