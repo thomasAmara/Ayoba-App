c4b9c1daf777f5e0e3b4ed75c055aaf565470ab7
thomasAmara <thomastarawali@gmail.com>
2023-07-10 00:13:08 +0100

    responsive design

--- src/components/Ayobapp.jsx
+++ src/components/Ayobapp.jsx
@@ -5,9 +5,13 @@ import Lady from '../Images/women_chopped_2bf54a3b3c 1.svg';
 import Ayoba from '../Images/ayobaLogo.png';
 import '../acc.css';
 import { useMediaQuery } from '@chakra-ui/react';
+import { FaApple } from 'react-icons/fa';
+import { FaGooglePlay } from 'react-icons/fa';
 
 function App() {
   const [isLessThan525] = useMediaQuery('(max-width: 525px)');
+  const [isLessThan415] = useMediaQuery('(max-width: 415px)');
+
   return (
     <div className='mainContainer'>
       <Box
@@ -24,16 +28,37 @@ function App() {
           {/* {isLessThan525 ? display: 'none' : 'flex'} */}
           <Image src={Lady} />
         </Box>
-        <Box display='flex' flexDirection='column'>
-          <Text
-            display='flex'
-            fontSize='30px'
-            fontFamily='Inter'
-            fontWeight='500'
-          >
-            Join the {` `} <Image ml='5px' mr='5px' src={Ayoba} />
-            {` `} family
-          </Text>
+        <Box
+          display='flex'
+          width={isLessThan525 ? '100%' : 'auto'}
+          p={isLessThan525 ? '15px' : '10px'}
+          flexDirection='column'
+        >
+          <Box display='flex' justifyContent='center'>
+            <Text
+              display='flex'
+              fontSize='30px'
+              fontFamily='Inter'
+              fontWeight='500'
+            >
+              Join the {` `}{' '}
+              <Text
+                ml='4px'
+                mr='4px'
+                display={!isLessThan415 ? 'none' : 'flex'}
+                color='#FDB933'
+              >
+                Ayoba
+              </Text>
+              <Image
+                ml='5px'
+                display={isLessThan415 ? 'none' : 'flex'}
+                mr='5px'
+                src={Ayoba}
+              />
+              {` `} family
+            </Text>
+          </Box>
           <Wrap
             mt='10px'
             pt='120px'
@@ -49,18 +74,32 @@ function App() {
                 width={['300px', '350px', '400px']}
                 height='60px'
                 colorScheme='facebook'
+                fontSize='24px'
+                fontFamily='Inter'
+                fontWeight='500'
+                display='flex'
+                justifyContent='space-evenly'
+                p='3px 15px'
               >
-                Download For Ios
+                <FaApple size={32} />
+                Download for iOS
               </Button>
             </WrapItem>
-            <WrapItem>
+            <WrapItem pb={['10px', '5px', '0px']}>
               <Button
                 borderRadius='7px'
                 width={['300px', '350px', '400px']}
                 height='60px'
                 colorScheme='facebook'
+                fontSize='24px'
+                fontFamily='Inter'
+                fontWeight='500'
+                display='flex'
+                justifyContent='space-evenly'
+                p='3px 15px'
               >
-                Download For Android
+                <FaGooglePlay size={32} mr='5px' />
+                Download for Android
               </Button>
             </WrapItem>
           </Wrap>
